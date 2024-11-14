15ca2e6ea6b84dbee89c43366deb02b6a7815042
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-11-14 15:39:01 +0200

    add arrays.php to demonstrate array creation and counting; update strings.php to include string length calculation

---
+++ playground/arrays.php
@@ -0,0 +1,7 @@
+<?php
+$list = array('one', 'two', 'three', 'four', 'five');
+$countries = ['USA', 'Canada', 'Mexico'];
+
+$length = count($list);
+
+echo $length;
--- playground/strings.php
+++ playground/strings.php
@@ -5,8 +5,10 @@ $title = 'PHP Fundamentals';
 $message = 'Welcome to ' . $title;
 $message1 = "Welcome to $title";
 
-
+$length = strlen($message);
 
 echo $message;
 echo "\n";
 echo $message1;
+echo "\n";
+echo $length;
